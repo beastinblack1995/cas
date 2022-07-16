25cfec5b8a5193130ce45ee6491bff69bcb549ae
beastinblack1995 <109196514+beastinblack1995@users.noreply.github.com>
2022-07-16 08:09:21 +0530

    Update app.py

--- app.py
+++ app.py
@@ -100,7 +100,8 @@ def gen_frames():  # generate frame by frame from camera
                 y1, x2, y2, x1 = y1*4, x2*4, y2*4, x1*4
                 cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 0), 3)
                 cv2.rectangle(frame, (x1, y2-25), (x2, y2), (0, 255, 0), cv2.FILLED)
-                cv2.putText(frame, name, (x1+6, y2-6), cv2.FONT_HERSHEY_COMPLEX, 1, (255, 255, 255), 2)
+                #cv2.putText(frame, name, (x1+6, y2-6), cv2.FONT_HERSHEY_COMPLEX, 1, (255, 255, 255), 2)
+                cv2.putText(frame, name, (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), 2)
                 MarkAttendence(name)
                 print(name)
 
