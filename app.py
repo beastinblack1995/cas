6aae1903c8ae4561b4f4db7614e5e62264c897fa
beastinblack1995 <109196514+beastinblack1995@users.noreply.github.com>
2022-07-16 08:31:30 +0530

    Update app.py

--- app.py
+++ app.py
@@ -96,14 +96,17 @@ def gen_frames():  # generate frame by frame from camera
 
 
                 name = employeeName[matchIndex].upper()
-                y1, x2, y2, x1 = faceloc
-                y1, x2, y2, x1 = y1*4, x2*4, y2*4, x1*4
-                cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 0), 3)
-                cv2.rectangle(frame, (x1, y2-25), (x2, y2), (0, 255, 0), cv2.FILLED)
-                cv2.putText(frame, name, (x1+6, y2-6), cv2.FONT_HERSHEY_COMPLEX, 1, (255, 255, 255), 2)
+                #y1, x2, y2, x1 = faceloc
+                #y1, x2, y2, x1 = y1*4, x2*4, y2*4, x1*4
+                #cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 0), 3)
+                #cv2.rectangle(frame, (x1, y2-25), (x2, y2), (0, 255, 0), cv2.FILLED)
+                #cv2.putText(frame, name, (x1+6, y2-6), cv2.FONT_HERSHEY_COMPLEX, 1, (255, 255, 255), 2)
+                startX, startY, endX, endY = faceloc
+                cv2.rectangle(image, (startX, startY), (endX, endY), (0, 255, 0), 2)
 
                 MarkAttendence(name)
                 print(name)
+                
 
         
         
